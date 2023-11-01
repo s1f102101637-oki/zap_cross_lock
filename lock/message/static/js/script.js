4096deb5e52acd50dcf2b954d8c853b953455c91
s1f102101637-oki <s1f102101637@iniad.org>
2023-11-02 04:12:38 +0900

    試行錯誤

--- lock/message/static/js/script.js
+++ lock/message/static/js/script.js
@@ -2,3 +2,13 @@
 $(".profile-card").click(function() {
     // Show message form or perform other actions
 });
+
+// JavaScript
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.profile-card').forEach(card => {
+        card.addEventListener('click', function() {
+            const name = this.getAttribute('data-name');
+            window.location.href = `/profile/${name}/`; // DjangoのURLルーティングに合わせて調整
+        });
+    });
+});
--- lock/message/views.py
+++ lock/message/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 
 def message_page(request):
   return render(request, 'message/index.html', {})
+
+def profile_view(request, name):
+    # 何らかの処理（例：データベースからプロフィール情報を取得）
+    return render(request, 'profile_template.html', {'name': name})
