e4922a8bd702b1c3c4505c5a715cbd647fd1dc7c
s1f102101637-oki <s1f102101637@iniad.org>
2023-11-02 02:39:16 +0900

    index.html反映

--- lock/message/views.py
+++ lock/message/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def root(request):
-  return HttpResponse('Hello Django')
+def message_page(request):
+  return render(request, 'message/index.html', {})
